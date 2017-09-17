b3afb8abb09f9f94310adbc33704d9fbaf4f49a4
Daniel Stucki <misc.daniel@stucki.me>
2017-09-17 02:29:47 +0200

    put together door opening/closing use case

--- Tinkerforge/TinkerforgeExample/src/tinkerforgeexample/NFCRFIDBricklet.java
+++ Tinkerforge/TinkerforgeExample/src/tinkerforgeexample/NFCRFIDBricklet.java
@@ -9,13 +9,15 @@ public class NFCRFIDBricklet {
     private final BrickletNFCRFID instance;
     private static final String UID = "utP";
     private short currentTagType = 0;
-	private TinkerforgeDemo tinkerforgeDemo;
+    private TinkerforgeDemo app;
+    private String lastProcessedTagId;
 
 
-    public NFCRFIDBricklet(TinkerforgeDemo tinkerforgeDemo, IPConnection ipcon) {
-        this.tinkerforgeDemo = tinkerforgeDemo;
-		this.instance = new BrickletNFCRFID(UID, ipcon);
+    public NFCRFIDBricklet(TinkerforgeDemo app, IPConnection ipcon) {
+        this.app = app;
+        this.instance = new BrickletNFCRFID(UID, ipcon);
     }
+
 
     public void registerLogic() throws TimeoutException, NotConnectedException {
         this.instance.addStateChangedListener((state, idle) -> {
@@ -26,22 +28,20 @@ public class NFCRFIDBricklet {
                 }
 
                 if (state == BrickletNFCRFID.STATE_REQUEST_TAG_ID_READY) {
-                    BrickletRGBLED.RGBValue rgbValue = tinkerforgeDemo.getLight().getRGBValue();
-                    StringBuilder s = readTagId();
-                    if ("47cc1f2794d80".equals(s.toString())) {
-                    	tinkerforgeDemo.getLight().setColor(Color.GREEN);;
+                    String currentTagId = readTagId();
+                    System.out.println("ctagid" + currentTagId);
+                    if ("47cc1f2794d80".equals(currentTagId)) {
+                        app.moveServo(true, 1000);
+                        app.blackOutLight();
+                        app.getLight().setColor(Color.GREEN, 1000);
                     } else {
-                    	tinkerforgeDemo.getLight().setColor(Color.RED);
+                        app.moveServo(false);
+                        app.blackOutLight();
+                        app.getLight().setColor(Color.RED, 1000);
                     }
-
-                    Thread.sleep(2000);
-                    tinkerforgeDemo.getLight().setRGBValue(rgbValue.r, rgbValue.b, rgbValue.g);
-
-                    System.out.println(s);
                 }
-
             } catch (Exception e) {
-                System.out.println(e.getMessage());
+                e.printStackTrace();
             }
         });
 
@@ -50,14 +50,14 @@ public class NFCRFIDBricklet {
 
     }
 
-    private StringBuilder readTagId() throws TimeoutException, NotConnectedException {
+    private String readTagId() throws TimeoutException, NotConnectedException {
         BrickletNFCRFID.TagID tagID = instance.getTagID();
         StringBuilder s = new StringBuilder();
 
         for (int i = 0; i < tagID.tidLength; i++) {
             s.append(Integer.toHexString(tagID.tid[i]));
         }
-        return s;
+        return s.toString();
     }
 
     public static void setTimeout(Runnable runnable, int delay) {
--- Tinkerforge/TinkerforgeExample/src/tinkerforgeexample/TinkerforgeDemo.java
+++ Tinkerforge/TinkerforgeExample/src/tinkerforgeexample/TinkerforgeDemo.java
@@ -6,105 +6,137 @@ package tinkerforgeexample;
 
 import com.tinkerforge.*;
 
+import java.awt.*;
+
 /**
  * A simple example showing how to create a simple Tinkerforge application using
  * a master brick and the following bricklets:
- * 
+ * <p>
  * - Dual button bricklet - RGB LED bricklet
- * 
+ * <p>
  * Note: Change the bricklet UID's to the actual UID's of your bricklet hardware
  * (see the "TODO's" below). Use Tinkerforge's Brick Daemon and Brick Viewer to
  * obtain the UID's.
- * 
  */
 public class TinkerforgeDemo {
 
-	private static final String HOST = "172.31.0.225";
-	private static final int PORT = 4223;
+    private static final String HOST = "172.31.0.225";
+    private static final int PORT = 4223;
 
-	private static final String DUAL_BUTTON_UID = "vTZ";
-	private static final String SERVO_UID = "6Rrbr9";
-	private static final short SERVO_NUMBER = 6;
+    private static final String DUAL_BUTTON_UID = "vTZ";
+    private static final String SERVO_UID = "6Rqvth";
+    private static final short SERVO_NUMBER = 0;
 
-	private static BrickletDualButton dualButtonBricklet;
-	private MotionDetector motionDetector;
-	private Light light;
-	private static BrickServo servoBricklet;
-	private static NFCRFIDBricklet nfcrfidBricklet;
-	
-	public TinkerforgeDemo() throws Exception{
-		IPConnection ipcon = new IPConnection();
-		motionDetector = new MotionDetector(this, ipcon);
-		light = new Light(this, ipcon);
-		dualButtonBricklet = new BrickletDualButton(DUAL_BUTTON_UID, ipcon);
-		servoBricklet = new BrickServo(SERVO_UID, ipcon);
-		nfcrfidBricklet = new NFCRFIDBricklet(this, ipcon);
+    private static BrickletDualButton dualButtonBricklet;
+    private MotionDetector motionDetector;
+    private Light light;
+    private BrickServo servoBricklet;
+    private static NFCRFIDBricklet nfcrfidBricklet;
 
-		ipcon.connect(HOST, PORT); // Connect to brickd
-		
-		motionDetector.startMotionDetection();
+    public TinkerforgeDemo() throws Exception {
+        IPConnection ipcon = new IPConnection();
+        motionDetector = new MotionDetector(this, ipcon);
+        light = new Light(this, ipcon);
+        dualButtonBricklet = new BrickletDualButton(DUAL_BUTTON_UID, ipcon);
+        servoBricklet = new BrickServo(SERVO_UID, ipcon);
+        nfcrfidBricklet = new NFCRFIDBricklet(this, ipcon);
 
-		nfcrfidBricklet.registerLogic();
+        ipcon.connect(HOST, PORT); // Connect to brickd
 
-		nfcrfidBricklet.registerLogic();
+        moveServo(false);
 
-		// Add state changed listener
-		dualButtonBricklet.addStateChangedListener(new BrickletDualButton.StateChangedListener() {
-			public void stateChanged(short buttonL, short buttonR, short ledL, short ledR) {
-				// This function is called when a button on the dual button
-				// bricklet is pressed.
-				System.out.println("dual button clicked.");
-				if (buttonL > 0) {
-					motionDetector.motionDetected();
-				}
-				if (buttonR > 0) {
-					try {
-						toggleRightDualButtonLed();
-					} catch (Exception e) {
-					}
-				}
-			}
-		});
+        motionDetector.startMotionDetection();
 
-		System.out.println("Press key to exit");
-		System.in.read();
-		ipcon.disconnect();
-	}
+        nfcrfidBricklet.registerLogic();
 
-	public static void main(String[] args) throws Exception {
-		new TinkerforgeDemo();
-	}
+        light.setColor(Color.BLACK);
+        // Add state changed listener
+        dualButtonBricklet.addStateChangedListener(new BrickletDualButton.StateChangedListener() {
+            public void stateChanged(short buttonL, short buttonR, short ledL, short ledR) {
+                // This function is called when a button on the dual button
+                // bricklet is pressed.
 
-	protected void onMotionDetected() throws TimeoutException, NotConnectedException, InterruptedException {
-		moveServo();
-		getLight().flash(5);
-	}
+                if (buttonL == 1 && buttonR == 1) {
+                    try {
+                        toggleServo();
+                    } catch (TimeoutException | NotConnectedException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-	private static void moveServo() throws TimeoutException, NotConnectedException {
-		short position = servoBricklet.getPosition(SERVO_NUMBER);
-		System.out.println("servo was " + position);
-		if (position < 0) {
-			servoBricklet.setPosition(SERVO_NUMBER, (short) 8000);
-		} else {
-			servoBricklet.setPosition(SERVO_NUMBER, (short) -8000);
-		}
-	}
+                System.out.println("dual button clicked." + buttonL + buttonR + ledL + ledR);
+                /*try {
+                    moveServo(buttonL > 0);
+                } catch (TimeoutException | NotConnectedException e) {
+                    e.printStackTrace();
+                }
 
-	/**
-	 * Toggle the LED in the right button of the dual button bricklet.
-	 */
-	private static void toggleRightDualButtonLed() throws Exception {
-		BrickletDualButton.LEDState ledState = dualButtonBricklet.getLEDState();
+                if (buttonR > 0) {
+                    try {
+                        toggleRightDualButtonLed();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }*/
+            }
+        });
 
-		if (ledState.ledR == BrickletDualButton.LED_STATE_ON) {
-			dualButtonBricklet.setLEDState(BrickletDualButton.LED_STATE_OFF, BrickletDualButton.LED_STATE_OFF);
-		} else {
-			dualButtonBricklet.setLEDState(BrickletDualButton.LED_STATE_OFF, BrickletDualButton.LED_STATE_ON);
-		}
-	}
+        System.out.println("Press key to exit");
+        System.in.read();
+        ipcon.disconnect();
+    }
 
-	public Light getLight() {
-		return light;
-	}
+    private void toggleServo() throws TimeoutException, NotConnectedException {
+        short position = servoBricklet.getPosition(SERVO_NUMBER);
+        moveServo(position < 0);
+    }
 
+    public static void main(String[] args) throws Exception {
+        new TinkerforgeDemo();
+    }
+
+    protected void onMotionDetected() throws TimeoutException, NotConnectedException, InterruptedException {
+        getLight().flash(5);
+    }
+
+    protected void moveServo(boolean open) throws TimeoutException, NotConnectedException {
+        if (open) {
+            servoBricklet.setPosition(SERVO_NUMBER, (short) 9000);
+        } else {
+            servoBricklet.setPosition(SERVO_NUMBER, (short) -700);
+        }
+    }
+
+    /**
+     * Toggle the LED in the right button of the dual button bricklet.
+     */
+    private static void toggleRightDualButtonLed() throws Exception {
+        BrickletDualButton.LEDState ledState = dualButtonBricklet.getLEDState();
+
+        if (ledState.ledR == BrickletDualButton.LED_STATE_ON) {
+            dualButtonBricklet.setLEDState(BrickletDualButton.LED_STATE_OFF, BrickletDualButton.LED_STATE_OFF);
+        } else {
+            dualButtonBricklet.setLEDState(BrickletDualButton.LED_STATE_OFF, BrickletDualButton.LED_STATE_ON);
+        }
+    }
+
+    public Light getLight() {
+        return light;
+    }
+
+    public void blackOutLight() {
+        this.light.setColor(Color.BLACK);
+    }
+
+    public void moveServo(boolean open, int timeToClose) throws TimeoutException, NotConnectedException {
+        moveServo(open);
+        new Thread(() -> {
+            try {
+                Thread.sleep(timeToClose);
+                moveServo(!open);
+            } catch (InterruptedException | NotConnectedException | TimeoutException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 }
